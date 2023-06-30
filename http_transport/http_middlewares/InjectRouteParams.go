919706a732c14c9448727db9b3b1c82817377cbe
PABOIS Gaël <gael.pabois@developpement-durable.gouv.fr>
2023-06-30 15:47:24 +0200

    up

--- http_transport/http_middlewares/InjectRouteParams.go
+++ http_transport/http_middlewares/InjectRouteParams.go
@@ -7,15 +7,22 @@ import (
 	"github.com/gpabois/goservice/http_transport"
 	"github.com/gpabois/goservice/middlewares"
 	"github.com/gpabois/gostd/result"
+	"github.com/gpabois/gostd/serde/decoder"
+	"github.com/gpabois/gostd/serde/norm"
 )
 
 // Inject route params into the endpoint request
 func InjectRouteParams[T any]() middlewares.FlowMiddleware {
 	return middlewares.ByFunc(func(in flow.Flow) result.Result[flow.Flow] {
 		r := http_transport.Flow_GetHttpRequest(in)
-		routeParams := mux.Vars(r)
+
 		endpointRequest := endpoint.Flow_GetEndpointRequest[T](in).Expect()
-		res := f(endpointRequest)
+		var routeParams map[string]any
+		for key, value := range mux.Vars(r) {
+			routeParams[key] = value
+		}
+		d := norm.NewDecoder(routeParams)
+		res := decoder.DecodeInto(d, &endpointRequest)
 		if res.HasFailed() {
 			return result.Result[flow.Flow]{}.Failed(res.UnwrapError())
 		}
