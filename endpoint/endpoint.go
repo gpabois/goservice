d6b0608273095b465a938387b93df8157a6c589f
PABOIS Gaël <gael.pabois@developpement-durable.gouv.fr>
2023-07-05 17:27:03 +0200

    up

--- endpoint/endpoint.go
+++ endpoint/endpoint.go
@@ -1,12 +1,10 @@
 package endpoint
 
 import (
-	"context"
-
 	"github.com/gpabois/gostd/result"
 )
 
 //go:generate mockery
 type Endpoint[Request any, Response any] interface {
-	Process(ctx context.Context, request Request) result.Result[Response]
+	Process(request Request) result.Result[Response]
 }
